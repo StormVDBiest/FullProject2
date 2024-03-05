f8e2b6e4b094782f0cbb831f8907d405bd6cb083
Leerlingen <Leerlingen@T2-7>
2024-03-05 10:51:43 +0100

    test

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,21 +1,14 @@
 ﻿using Smartcrop;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Processing;
+
 namespace Tests
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            /*Stream inStream = new(Stream.Null());
-            using (Image image = Image.Load(inStream))
-            {
-                int width = image.Width / 2;
-                int height = image.Height / 2;
-                image.Mutate(x => x.Resize(width, height));
-
-                image.Save(outPath);
-            }*/
+            
 
         }
     }
